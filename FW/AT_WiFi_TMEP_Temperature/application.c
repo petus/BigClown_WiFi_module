d6df5b607561f0908a4577a7388e3e221e3ab47b
petus <petus@users.noreply.github.com>
2019-03-23 21:18:37 +0100

    Update application.c

--- FW/AT_WiFi_TMEP_Temperature/application.c
+++ FW/AT_WiFi_TMEP_Temperature/application.c
@@ -1,3 +1,18 @@
+/*
+* WiFi module with built-in temperature TMP112
+* Temperature TMP112 sensor (I2C0) is built-in BigClown Core module
+* and the sensor sends temperature to BigClown and then the BigClown 
+* sends the temperature to tmep.eu or tmep.cz by WiFi conection (WiFi module)
+* CH_EN pin of ESP8266 module is connected GPIO_P8
+* Reset pin of ESP8266 module is connected to GPIO_P6
+*
+* Created by Petus (2019)
+* (czech)
+* WiFi module https://chiptron.cz/articles.php?article_id=182
+* (english)
+* WiFi module https://time4ee.com/articles.php?article_id=113
+*/
+
 #include <application.h>
 #include <stdio.h>
 #include <string.h>
@@ -10,7 +25,7 @@ bc_led_t led;
 bc_tmp112_t temp;
 volatile float temperature = 0.0;
 
-static char host[]="NAME.tmep.eu";
+static char host[]="HOSTNAME.tmep.eu"; // or HOSTNAME.tmep.cz
 static char url[]="GET /index.php?tempC=";
 static char SSID[]="\"SSID\",";
 static char PASSWORD[]="\"PASSWORD\"";
